b4a28c766391f99956941ed2600929686ea94019
Chema Muñoz <chemunoz78@gmail.com>
2026-04-18 13:03:33 +0200

    refactor: remove tutorials, keep only pokemon feature

--- angular-17-client/src/app/app-routing.module.ts
+++ angular-17-client/src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
-import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
-import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'pokemon', pathMatch: 'full' },
-  { path: 'pokemon', component: PokemonListComponent },
-  { path: 'tutorials', component: TutorialsListComponent },
-  { path: 'tutorials/:id', component: TutorialDetailsComponent },
-  { path: 'add', component: AddTutorialComponent }
+  { path: 'pokemon', component: PokemonListComponent }
 ];
 
 @NgModule({
