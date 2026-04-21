e43b4909249acff13eda49a7f358ce62e373b27f
Chema Muñoz <chemunoz78@gmail.com>
2026-04-21 12:04:52 +0200

    refactor: migrate to standalone components and functional routing

---
+++ angular-pokemon-client/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { AddPokemonComponent } from './components/add-pokemon/add-pokemon.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'pokemon', pathMatch: 'full' },
+  { path: 'pokemon', component: PokemonListComponent },
+  { path: 'add', component: AddPokemonComponent }
+];
